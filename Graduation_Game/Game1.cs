f222d5383c9dfe08e9543ad5d13c5be5c8b273b3
lineeam <72556262+lineeam@users.noreply.github.com>
2022-05-16 22:35:52 +0200

    add basic player movement
    - player is able to move to the side and jump
    - x frame boundaries and y floor boundary
    - temporary floor and jump cooldown (instead of collision jump)
    - sprite flips depending on walking direction

--- Graduation_Game/Game1.cs
+++ Graduation_Game/Game1.cs
@@ -9,6 +9,17 @@ namespace Graduation_Game
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D playerSprite;
+        Texture2D playerLeftSprite;
+        Texture2D floorSprite;
+
+        SpriteFont font;
+
+        int screenWidth;
+        int screenHeight;
+
+        Player player = new Player();
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,7 +29,10 @@ namespace Graduation_Game
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            //_graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -26,6 +40,13 @@ namespace Graduation_Game
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            playerSprite = Content.Load<Texture2D>("player");
+            floorSprite = Content.Load<Texture2D>("caveFloorFull");
+            playerLeftSprite = Content.Load<Texture2D>("playerL");
+            font = Content.Load<SpriteFont>("timerFont");
+
+            screenWidth = GraphicsDevice.Viewport.Width;
+            screenHeight = GraphicsDevice.Viewport.Height;
 
             // TODO: use this.Content to load your game content here
         }
@@ -35,6 +56,7 @@ namespace Graduation_Game
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            player.playerUpdate(gameTime);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -42,7 +64,28 @@ namespace Graduation_Game
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LightGray);
+
+            _spriteBatch.Begin();
+
+            // Draws Player facing éither left or right
+            if(player.getIsFacingRight() == true)
+            {
+                _spriteBatch.Draw(playerSprite, new Vector2(player.getPosition().X - 15, player.getPosition().Y - 25), Color.White);
+            }
+            else
+            {
+                _spriteBatch.Draw(playerLeftSprite, new Vector2(player.getPosition().X - 15, player.getPosition().Y - 25), Color.White);
+            }
+            
+            // Draws floor 
+            for (int position = 0; position <= 1280; position += 32)
+            {
+                _spriteBatch.Draw(floorSprite, new Vector2(position -16, screenHeight - 32), Color.White);
+            }
+
+            _spriteBatch.DrawString(font, player.vy.ToString(), new Vector2(100, 100), Color.Black);
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
---
+++ Graduation_Game/Player.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Graduation_Game
+{
+    class Player
+    {
+        private Vector2 position = new Vector2(15, 662);
+        private int speed = 180;
+        private double health;
+        public float vy;
+        private double jumpingTimer;
+
+
+        public bool isFacingRight = true;
+
+        public Vector2 getPosition()
+        {
+            return this.position;
+        }
+
+        public void setPosition(Vector2 vec)
+        {
+            this.position = vec;
+        }
+
+        public void playerUpdate(GameTime gameTime)
+        {
+            // subtracting elapsed time from jump cooldown
+            jumpingTimer -= gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            KeyboardState kState = Keyboard.GetState();
+
+            //delta time
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //update velocity on Y axis
+            this.vy = this.vy < speed * 1.5 ? this.vy + 7.5f : this.vy;
+
+            //check if player above ground
+            if(position.Y + this.vy * dt < 662)
+            {
+                position.Y += this.vy * dt;
+            }
+            else
+            {
+                position.Y = 662;
+            }
+
+            //checking keystates
+            if (kState.IsKeyDown(Keys.Right) && position.X < 1265)
+            {
+                position.X += speed * dt;
+                isFacingRight = true; 
+            }
+            if (kState.IsKeyDown(Keys.Left) && position.X > 15)
+            {
+                position.X -= speed * dt;
+                isFacingRight = false ;
+            }
+            if (kState.IsKeyDown(Keys.Up))
+            {
+                // check if jump cooldown has expired
+                if (jumpingTimer <= 0)
+                {
+                    this.vy = -speed * 1.5f;
+                    jumpingTimer = 1200;
+                }
+                //position.Y -= speed * dt;
+                
+            }
+            if(position.Y > 662)
+            {
+                position.Y = 662;
+
+            }
+                
+            
+
+        }
+
+        public bool getIsFacingRight()
+        {
+            return isFacingRight;
+        }
+    }
+}
