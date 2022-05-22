c08ccc05eb8bb99f19000db2bd630d4f1aec634c
stefanuntura <stefan.untura@student.nhlstenden.com>
2022-05-22 22:16:23 +0200

    DEV-3: Add BossOne class
    - Add BossOne class implementing interface
    - Add Texture2D variable in the class
    - Add Vector2D variable in the class

---
+++ Graduation_Game/Entities/Bosses/BossOne.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graduation_Game.Entities.Bosses
+{
+    class BossOne : Entity
+    {
+        public Texture2D Sprite { get; set; }
+        public Vector2 Position { get; set; }
+        public int DrawOder { get; set; }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
