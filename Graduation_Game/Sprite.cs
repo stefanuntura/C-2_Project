ce6d7fdb83123f0b70cea9a5c6977ff762686feb
stefanuntura <stefan.untura@student.nhlstenden.com>
2022-05-22 22:39:23 +0200

    DEV-3: Modify Sprite class
    - Simplify Sprite class

--- Graduation_Game/Sprite.cs
+++ Graduation_Game/Sprite.cs
@@ -6,30 +6,20 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Graduation_Game.Graphics
 {
-    class Sprite
+    public class Sprite
     {
         public Texture2D Texture { get; private set; }
 
-        public int X { get; set; }
-        public int Y { get; set; }
-
-        public int Width { get; set; }
-        public int Height { get; set; }
-
         public Color TintColor { get; set; } = Color.White;
 
-        public Sprite(Texture2D texture, int x, int y, int width, int height)
+        public Sprite(Texture2D texture)
         {
             Texture = texture;
-            X = x;
-            Y = y;
-            Width = width;
-            Height = height;
         }
 
         public void Draw(SpriteBatch spriteBatch, Vector2 position)
         {
-            spriteBatch.Draw(Texture, position, new Rectangle(X, Y, Width, Height), TintColor);
+            spriteBatch.Draw(Texture, position, TintColor);
         }
     }
 }
