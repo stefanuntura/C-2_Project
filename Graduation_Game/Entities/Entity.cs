0eb8a1556c7d472152b6852fc15d132263937a1b
stefanuntura <stefan.untura@student.nhlstenden.com>
2022-05-22 22:09:28 +0200

    DEV-3: Add Entity interface
    - Add Entities folder
    - Add Entity Interface

---
+++ Graduation_Game/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graduation_Game.Entities
+{
+    interface Entity
+    {
+        int DrawOder { get; }
+
+        void Update(GameTime gameTime);
+
+        void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+    }
+}
