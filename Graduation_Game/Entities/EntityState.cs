ff40ecdb2673b7d6e8b3833f29d9d920f3e5283a
stefanuntura <stefan.untura@student.nhlstenden.com>
2022-05-22 22:16:51 +0200

    DEV-3: Add EntityState enum
    - Add EntityState enum class

---
+++ Graduation_Game/Entities/EntityState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graduation_Game.Entities
+{
+    public enum EntityState
+    {
+        Idle,
+        Running,
+        Walking,
+        Jumping,
+        Ducking,
+        Falling
+    }
+}
