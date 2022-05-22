5b99260518f3d2fcdff2ce1892c5019e018784f0
stefanuntura <stefan.untura@student.nhlstenden.com>
2022-05-22 21:52:16 +0200

    DEV-3: Add general Sprite class
    - Add Sprite class

--- Graduation_Game/Program.cs
+++ Graduation_Game/Program.cs
@@ -7,7 +7,7 @@ namespace Graduation_Game
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Game())
                 game.Run();
         }
     }
